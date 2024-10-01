67eeca3322ebb4c383372ad88a4c024bbba1c29f
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2024-10-02 00:43:58 +0200

    Update read.js
    Extended read.js with functionality to fetch data for a single post based on the post ID

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -19,3 +19,24 @@ export async function getPosts() {
 
   return data;
 }
+
+// Funksjon for å hente et enkelt innlegg basert på ID
+export async function getSinglePost(postId) {
+  const authToken = localStorage.getItem("authToken");
+  const apiKey = localStorage.getItem("apiKey");
+
+  const options = {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      "X-Noroff-API-Key": apiKey,
+    },
+  };
+
+  const response = await fetch(
+    `https://v2.api.noroff.dev/social/posts/${postId}`,
+    options
+  );
+  const data = await response.json();
+
+  return data;
+}
